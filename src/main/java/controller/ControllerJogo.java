046f3bc451ba9dc421af57a8fc00ed433ccaa5c6
Ana Paula <104795787+nanapssz@users.noreply.github.com>
2025-09-20 22:00:21 -0300

    ajustando nome do metodo ehFaseDeColocacao

--- src/main/java/controller/ControllerJogo.java
+++ src/main/java/controller/ControllerJogo.java
@@ -21,7 +21,8 @@ public class ControllerJogo {
         return jogo.getJogadorAtual();
     }
 
-    public boolean isPlacingPhase() {
-        return jogo.ehFaseDeColocacaoe();
+    public boolean ehFaseDeColocacao() {
+        return jogo.ehFaseDeColocacao();
     }
 }
+
